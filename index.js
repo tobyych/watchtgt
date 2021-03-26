b480cc943c9c727efa6c9416b137d956a7a346f8
Toby Yue <tobyych@gmail.com>
2021-03-26 19:49:52 +0800

    restructure project for heroku deployment

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-const port = 3001;
+const port = 5000;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-  // Request methods you wish to allow
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 io.sockets.on("connection", (socket) => {
   console.log("a user connected");
@@ -60,6 +38,10 @@ io.sockets.on("connection", (socket) => {
     console.log("a user disconnected");
   });
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
+})
 
 server.listen(port, () => {
   console.log(`Example app listening at port http://localhost:${port}`);
